5d3692386daae8c3041a5c8ec0f8310cc2d2caf1
Yennifer Pernia <perniayennifer@gmail.com>
2017-10-13 10:49:14 -0300

    Se agrega el alert cuando hay datos

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { AlertController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: Http) {
+  constructor(public http: Http,
+              public alert: AlertController) {
     console.log('Hello AuthProvider Provider');
   }
 
@@ -18,6 +20,15 @@ export class AuthProvider {
     });
     return this.http.post( this.funcionariosURL, body, { headers })
       .map( res => {
+        let dateRes = res.json();
+          if(dateRes == '') {
+            this.alert.create({
+              title:'No se encontraron Datos con este bp',
+              buttons:['OK']
+            }).present();
+          }else{
+
+          }
         console.log(res.json());
       });
   }
