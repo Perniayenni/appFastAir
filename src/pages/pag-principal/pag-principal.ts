afdbd8314b7446541c8dda0f52f8c81a4a758cd8
Yennifer Pernia <perniayennifer@gmail.com>
2017-10-13 15:42:57 -0300

    Se agrega pagina Principal

---
+++ src/pages/pag-principal/pag-principal.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
+
+@Component({
+  selector: 'page-pag-principal',
+  templateUrl: 'pag-principal.html',
+})
+export class PagPrincipalPage {
+  DatosFuncionarios:any=[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthProvider) {
+      this.DatosFuncionarios = this.auth.DatosFun;
+      console.log(this.DatosFuncionarios);
+  }
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PagPrincipalPage');
+  }
+
+}
