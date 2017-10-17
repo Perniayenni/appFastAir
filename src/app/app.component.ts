09c16756cef5ad7155898aff5679c18806a90cdb
Yennifer Pernia <perniayennifer@gmail.com>
2017-10-17 14:14:30 -0300

    Modificaciones de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,29 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth'
 
 import { HomePage } from '../pages/home/home';
+import { PagPrincipalPage} from '../pages/pag-principal/pag-principal';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              private auth:AuthProvider) {
     platform.ready().then(() => {
+
+      this.auth.cargarStorageSession();
+      console.log(this.auth.sessionStart);
+
+      if(this.auth.sessionStart){
+        this.rootPage = PagPrincipalPage;
+      }else{
+        this.rootPage = HomePage;
+      }
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -12,6 +12,15 @@ import 'rxjs/add/operator/map';
 export class AuthProvider {
 
   msg:mensaje[]=[];
+  userID:string= "";
+
+  funRegistro:string= '';
+  DatosFun:any=[];
+  sessionStart:boolean = false;
+
+  funcionariosURL:string = 'http://fastair.ourproject.cl/public/aut';
+  sendOneSing:string = 'https://fastair.ourproject.cl/public/credenciales';
+
 
   constructor(public http: Http,
               public alert: AlertController,
@@ -21,12 +30,6 @@ export class AuthProvider {
     this.cargarMensajesStorage();
   }
 
-  funRegistro:string= '';
-  DatosFun:any=[];
-  sessionStart:boolean = false;
-
-  funcionariosURL:string = 'http://fastair.ourproject.cl/public/aut';
-  sendOneSing:string = 'https://fastair.ourproject.cl/public/credenciales';
 
 
   getFuncionario(datos) {
@@ -44,96 +47,154 @@ export class AuthProvider {
               title:'No se encontraron Datos con este bp',
               buttons:['OK']
             }).present();
+            this.sessionStart=false;
           }else{
             this.DatosFun = dateRes;
+            this.sessionStart= true;
             for (let re of dateRes) {
               this.funRegistro = re.registro;
             }
-            this.updatePlayerID(this.getOneSignal());
+            console.log(this.sessionStart);
+            this.guardamosStorageSession();
 
           }
-        console.log(this.DatosFun);
-        console.log(this.funRegistro);
       });
   }
 
-  getOneSignal():any{
-    let userID;
-    this.oneSignal.startInit('a9ade8e0-a97b-4493-9e90-ed8c03cf3a75', '520421720191');
+    getOneSignal(){
+
+      this.oneSignal.startInit('b4872d1c-8bae-4355-aef6-6bec584fc6af', '1076348014901');
 
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
-    this.oneSignal.handleNotificationReceived().subscribe(() => {
-      // do something when notification is received
+    this.oneSignal.handleNotificationReceived().subscribe(data => {
+      let resp = data.payload;
+
+      ///////// Creamos La fecha Actual ////////
+      let FechaActual = new Date();
+      let hora =FechaActual.getHours()+":"+FechaActual.getMinutes()+":"+FechaActual.getSeconds();
+      let fechaA = FechaActual.getDate()+"-"+FechaActual.getMonth()+"-"+FechaActual.getFullYear();
+      let fechaCompleta = fechaA+" "+hora;
+
+      ///////// guardamos en la clase ////////
+      let mensag = new mensaje (fechaCompleta, resp.title, resp.body )
+      this.msg.push(mensag);
+
+      ///////// Guardamos en LocalStorage ////////
+      this.guardarMensajesStorage();
     });
 
     this.oneSignal.handleNotificationOpened().subscribe(data => {
-      alert(data.notification);
-      let resp = data.notification;
-      let momentoActual = new Date();
-      let fechaA =momentoActual.getHours()+"-"+momentoActual.getMinutes()+"-"+momentoActual.getSeconds();
-      let mensag = new mensaje (fechaA, resp.payload.title, resp.payload.body );
-      this.msg.push(mensag);
-      this.guardarMensajesStorage();
-
+      alert(data.notification.payload.body);
     });
+
     this.oneSignal.getIds().then( (datos) =>{
-      return userID = datos.userId;
+       this.updatePlayerID(datos.userId);
+
     });
 
     this.oneSignal.endInit();
 
   }
 
-  updatePlayerID(idOne){
-    let url = this.sendOneSing+"/"+this.funRegistro;
-    let body = {
-      "playerID":idOne
-    };
-    let headers = new Headers({
-      'Content-Type':'application/json'
-    });
-
-    return this.http.put( url, body, { headers })
-      .map( res => {
-        console.log(res.json());
+    updatePlayerID(idOne){
+    console.log("Entro en update");
+    console.log(idOne);
+      let url = this.sendOneSing+"/"+this.funRegistro;
+      let body = {
+        "playerID":idOne
+      };
+      let headers = new Headers({
+        'Content-Type':'application/json'
       });
-  }
 
- private guardarMensajesStorage(){
+      return this.http.put( url, body, { headers })
+        .map( res => {
+          console.log(res.json());
+        });
+    }
+
+    private guardarMensajesStorage(){
+        if (this.platform.is('cordova')){
+          this.storage.set('mensajes', this.msg);
+        }else{
+          // computadora
+          localStorage.setItem('mensajes', JSON.stringify(this.msg));
+        }
+    }
+
+    cargarMensajesStorage(){
+
+      let promesa = new  Promise ( (resolve, reject) => {
+        if (this.platform.is('cordova')){
+          //Dispositivo
+          this.storage.ready()
+            .then(()=>{
+
+            this.storage.get('mensajes')
+                .then(mesajes =>{
+                  if(mesajes){
+                    this.msg =mesajes;
+                  }
+                  resolve();
+                })
+            });
+        }else{
+          //Computadora
+          if (localStorage.getItem('mensajes')){
+            this.msg = JSON.parse(localStorage.getItem('mensajes'))
+          }
+          resolve();
+        }
+
+      });
+
+    }
+
+    private guardamosStorageSession(){
       if (this.platform.is('cordova')){
-        this.storage.set('mensajes', this.msg);
+        this.storage.set('SessionStart', this.sessionStart);
+        this.storage.set('funcionarios', this.DatosFun);
       }else{
         // computadora
-        localStorage.setItem('mensajes', JSON.stringify(this.msg));
+        localStorage.setItem('SessionStart', JSON.stringify(this.sessionStart));
+        localStorage.setItem('funcionarios', JSON.stringify(this.DatosFun));
       }
-  }
 
-  cargarMensajesStorage(){
+    }
 
-    let promesa = new  Promise ( (resolve, reject) => {
-      if (this.platform.is('cordova')){
-        //Dispositivo
-        this.storage.ready()
-          .then(()=>{
+    public cargarStorageSession(){
+      let promesa = new  Promise ( (resolve, reject) => {
+        if (this.platform.is('cordova')){
+          //Dispositivo
+          this.storage.ready()
+            .then(()=>{
 
-          this.storage.get('mensajes')
-              .then(mesajes =>{
-                if(mesajes){
-                  this.msg =mesajes;
-                }
-                resolve();
-              })
-          });
-      }else{
-        //Computadora
-        if (localStorage.getItem('mensajes')){
-          this.msg = JSON.parse(localStorage.getItem('mensajes'))
+              this.storage.get('SessionStart')
+                .then(res =>{
+                  if(res){
+                    this.sessionStart = res;
+
+                  }
+                  resolve();
+                })
+
+              this.storage.get('funcionarios')
+                .then(res =>{
+                  if(res){
+                    this.DatosFun = res;
+                  }
+                  resolve();
+                })
+            });
+        }else{
+          //Computadora
+          if (localStorage.getItem('SessionStart')){
+            this.sessionStart = JSON.parse(localStorage.getItem('SessionStart'));
+            this.DatosFun = JSON.parse(localStorage.getItem('funcionarios'));
+          }
+          resolve();
         }
-        resolve();
-      }
 
-    });
-
-  }
-
+      });
+    }
 }
