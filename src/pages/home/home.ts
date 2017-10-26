022e624a15e471497ea74a3f0167dad32d0c53c8
Yennifer Pernia <perniayennifer@gmail.com>
2017-10-26 16:12:24 -0300

    Validacion del formato del Rut

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,4 +33,33 @@ export class HomePage {
     } )
   }
 
+  verificarEntrada() {
+    let rut1 = this.rut;
+    let rut2 = rut1.split('.').join('');
+    let rut = rut2.split('-').join('');
+    let sRut1 = rut;      // contador de para saber cuando insertar el . o la -
+    let nPos = 0; // Guarda el rut invertido con los puntos y el guión agregado
+    let sInvertido = ''; // Guarda el resultado final del rut como debe ser
+    let sRut = '';
+    for (var i = sRut1.length - 1; i >= 0; i-- ) {
+      sInvertido += sRut1.charAt(i);
+      if (i == sRut1.length - 1 )
+        sInvertido += '-';
+      else if (nPos == 3)
+      {
+        sInvertido += '.';
+        nPos = 0;
+      }
+      nPos++;
+    }
+    for(var j = sInvertido.length-1; j>=0; j-- )
+    {
+      if (sInvertido.charAt(sInvertido.length - 1) != '.')
+        sRut += sInvertido.charAt(j);
+      else if (j != sInvertido.length - 1 )
+        sRut += sInvertido.charAt(j);
+    }
+    // Pasamos al campo el valor formateado
+    this.rut= sRut;
+  }
 }
